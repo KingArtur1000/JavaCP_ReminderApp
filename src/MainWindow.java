d13257f4a43749ce2627a273cfa6ee94a0bb2086
Артур Александрович <artur.dm.2005@yandex.ru>
2025-10-01 22:14:32 +0300

    Update MainWindow.java

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -6,15 +6,18 @@ public class MainWindow {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            // Параметры окна
             JFrame frame = new JFrame("Reminder App");
             frame.setSize(800, 600);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLocationRelativeTo(null);
 
+
             // Левая часть: календарь
             JCalendar calendar = new JCalendar();
             JPanel calendarPanel = new JPanel(new BorderLayout());
             calendarPanel.add(calendar, BorderLayout.CENTER);
+
 
             // Правая часть: текст + кнопка
             JTextArea textArea = new JTextArea("Rostiiiiick");
@@ -24,12 +27,13 @@ public class MainWindow {
 
             JButton saveButton = new JButton("Сохранить");
 
-            // Правое окно: сверху - окно ввода текста, снизу - кнопка сохранения
+            // Разделитель правой части: сверху - окно ввода текста, снизу - кнопка сохранения
             JSplitPane rightPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, scrollPane, saveButton);
             rightPane.setResizeWeight(0.85); // 80% сверху, 20% снизу
             rightPane.setOneTouchExpandable(false);
 
-            // SplitPane: делим окно на 2 части
+
+            // Главный SplitPane: делим окно на 2 части:
             JSplitPane splitPane = new JSplitPane(
                     JSplitPane.HORIZONTAL_SPLIT,
                     calendarPanel,
@@ -38,6 +42,7 @@ public class MainWindow {
             splitPane.setResizeWeight(0.7); // 70% слева, 30% справа
             splitPane.setOneTouchExpandable(false); // стрелочки для сворачивания
 
+            // Добавляем на форму основной splitPane и отображаем на форме :)
             frame.add(splitPane, BorderLayout.CENTER);
             frame.setVisible(true);
         });
